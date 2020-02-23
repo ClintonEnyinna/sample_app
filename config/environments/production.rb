094c4c83cbb219b0adf21c0ab843c3b42d6a8c6f
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-02-23 03:36:48 -0600

    correct type

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  onfig.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'https://fast-tundra-08760.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
