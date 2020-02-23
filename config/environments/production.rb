038c85051cf149a1c6d8f001fd5c3a38aec2789c
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-02-23 01:34:30 -0600

    add api key to sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,13 +66,13 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'arcane-meadow-13276.herokuapp.com'
+  config.action_mailer.raise_delivery_errors = true
+  host = 'intense-ridge-87478.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+    :port           => '465',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
